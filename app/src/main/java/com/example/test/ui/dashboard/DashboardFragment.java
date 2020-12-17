def61390464ad54004eb2cd31dfa0b175f2c582f
Wrist24 <ristich_stefan@yahoo.com>
2020-12-17 13:07:43 +0100

    'bezveze'

--- app/src/main/java/com/example/test/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/test/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,22 +14,36 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.test.R;
 
+import java.io.Console;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private String kombinacija;
+    private Button kopce;
+    private View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        /*final TextView textView = root.findViewById(R.id.materijal_tekst);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        //EditText materijal = getView().findViewById(R.id.materijal_tekst);
+        //materijal.setFocusable(false);
         return root;
     }
+    kopce = getActivity().findViewById(R.id.plastika);
+    kopce.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void stringBuilder(View view)
+        {
+            kombinacija += view.getId();
+        }
+    });
 }
